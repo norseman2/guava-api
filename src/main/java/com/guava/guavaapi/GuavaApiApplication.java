79d135c550e2bf162e1bd8bd1405b852aa0310f0
Frederic Ory <fred@systemike.com>
2020-08-01 17:54:32 -0400

    java guavi api init

---
+++ src/main/java/com/guava/guavaapi/GuavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.guava.guavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guava/guavaapi/GuavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guava.guavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
