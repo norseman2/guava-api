b3254dbbed836ac1e71ed79d8f331a6f0adbcc84
Frederic Ory <fred@systemike.com>
2020-08-02 23:47:26 -0400

    guava api first code

--- src/main/java/com/guava/api/GuavaApiApplication.java
+++ src/main/java/com/guava/api/GuavaApiApplication.java
@@ -1,13 +1,19 @@
-package com.guava.guavaapi;
+package com.guava.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class GuavaApiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(GuavaApiApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(GuavaApiApplication.class, args);
+		logger.info("Spring Boot guava-api launched");
 	}
 
 }
--- src/test/java/com/guava/api/GuavaApiApplicationTests.java
+++ src/test/java/com/guava/api/GuavaApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.guava.guavaapi;
+package com.guava.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
